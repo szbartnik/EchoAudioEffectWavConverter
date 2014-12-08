ebbecb71aad64012ca0f7b30fa6bc24e6303ffe4
Szymon Bartnik <szymbar099@student.polsl.pl>
2014-12-08 19:18:18 +0100

    Update of app.

--- CppLib/CppLib.h
+++ CppLib/CppLib.h
@@ -2,4 +2,7 @@
 
 #define API extern "C" __declspec(dllexport)
 
+void Process_Data(void);
+void EchoSample(void);
+int GetFromBuffer(int index);
 API void ProcessCpp(short* lChannel, short* rChannel, int dataSize);
